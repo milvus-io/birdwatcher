e2d02cf41c32d8bc9949906bbc928e6d93c5ca79
Congqi Xia <congqi.xia@zilliz.com>
2022-07-11 16:36:51 +0800

    Fix conflict solving remains
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/etcd_backup.go
+++ states/etcd_backup.go
@@ -95,7 +95,6 @@ func getBackupEtcdCmd(cli *clientv3.Client, basePath string) *cobra.Command {
 
 // backupEtcd backup all key-values with prefix provided into local file.
 // implements gzip compression for now.
-<<<<<<< HEAD
 func backupEtcd(cli *clientv3.Client, base, prefix string, component string, filePath string, ignoreRevision bool) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
