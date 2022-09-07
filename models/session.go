9b2597358dfaca586e87a9a7363e1290b45f987b
zhenshan.cao <zhenshan.cao@zilliz.com>
2022-09-07 10:04:30 +0800

    Add cmd for show querycoord cluster info (#22)
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>
    
    Signed-off-by: zhenshan.cao <zhenshan.cao@zilliz.com>

--- models/session.go
+++ models/session.go
@@ -1,9 +1,15 @@
 package models
 
+import "fmt"
+
 // Session is the json model for milvus session struct in etcd.
 type Session struct {
 	ServerID   int64  `json:"ServerID,omitempty"`
 	ServerName string `json:"ServerName,omitempty"`
 	Address    string `json:"Address,omitempty"`
 	Exclusive  bool   `json:"Exclusive,omitempty"`
+}
+
+func (s Session) String() string {
+	return fmt.Sprintf("Session:%s, ServerID: %d", s.ServerName, s.ServerID)
 }
