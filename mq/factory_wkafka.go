8a8fb4a235661332d80a1ba801c222a5a1b4a468
congqixia <congqi.xia@zilliz.com>
2023-02-21 11:56:23 +0800

    Add WKAFKA build flag for control cgo(kafka) feature (#96)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- mq/factory_wkafka.go
+++ mq/factory_wkafka.go
@@ -1,5 +1,5 @@
-//go:build !WKAFKA
-// +build !WKAFKA
+//go:build WKAFKA
+// +build WKAFKA
 
 package mq
 
@@ -8,12 +8,15 @@ import (
 	"time"
 
 	"github.com/milvus-io/birdwatcher/mq/ifc"
+	"github.com/milvus-io/birdwatcher/mq/kafka"
 	"github.com/milvus-io/birdwatcher/mq/pulsar"
 )
 
 func NewConsumer(mqType, address, channel string) (ifc.Consumer, error) {
 	groupID := fmt.Sprintf("group-id-%d", time.Now().UnixNano())
 	switch mqType {
+	case "kafka":
+		return kafka.NewKafkaConsumer(address, channel, groupID)
 	case "pulsar":
 		return pulsar.NewPulsarConsumer(address, channel, groupID)
 	default:
