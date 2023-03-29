12f334c6a7b244d5b3730390528402e20553606e
Congqi Xia <congqi.xia@zilliz.com>
2023-03-29 15:02:39 +0800

    Support show segment distribution by collection
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/distribution.go
+++ states/distribution.go
@@ -13,11 +13,16 @@ import (
 	"google.golang.org/grpc"
 )
 
+// GetDistributionCommand returns command to iterate all querynodes to list distribution.
 func GetDistributionCommand(cli clientv3.KV, basePath string) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "segment-loaded-grpc",
 		Short: "list segments loaded infomration",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			collectionID, err := cmd.Flags().GetInt64("collection")
+			if err != nil {
+				return err
+			}
 			sessions, err := common.ListSessions(cli, basePath)
 			if err != nil {
 				return err
@@ -47,27 +52,41 @@ func GetDistributionCommand(cli clientv3.KV, basePath string) *cobra.Command {
 					})
 					if err != nil {
 						fmt.Println(err.Error())
-						return err
+						continue
 					}
 
 					// print channel
 					for _, channel := range resp.GetChannels() {
+						if collectionID != 0 && channel.GetCollection() != collectionID {
+							continue
+						}
 						fmt.Printf("Channel %s, collection: %d, version %d\n", channel.Channel, channel.Collection, channel.Version)
 					}
 
 					for _, lv := range resp.GetLeaderViews() {
+						if collectionID != 0 && lv.GetCollection() != collectionID {
+							continue
+						}
 						fmt.Printf("Leader view for channel: %s\n", lv.GetChannel())
 						growings := lv.GetGrowingSegmentIDs()
 						fmt.Printf("Growing segments number: %d , ids: %v\n", len(growings), growings)
 					}
 
-					fmt.Printf("Node Loaded Segments number: %d\n", len(resp.GetSegments()))
-
+					sealedNum := 0
+					for _, segment := range resp.GetSegments() {
+						if collectionID != 0 && segment.GetCollection() != collectionID {
+							continue
+						}
+						fmt.Printf("SegmentID: %d CollectionID: %d Channel: %s\n", segment.GetID(), segment.GetCollection(), segment.GetChannel())
+						sealedNum++
+					}
+					fmt.Println("Sealed segments number:", sealedNum)
 				}
 			}
 
 			return nil
 		},
 	}
+	cmd.Flags().Int64("collection", 0, "collection id to filter with")
 	return cmd
 }
