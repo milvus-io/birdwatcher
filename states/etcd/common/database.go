a87d7b00e6b28624166114c7b31bee5257fa0e39
congqixia <congqi.xia@zilliz.com>
2023-06-06 16:56:32 +0800

    Add show databases command (#149)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

---
+++ states/etcd/common/database.go
@@ -0,0 +1,33 @@
+package common
+
+import (
+	"context"
+	"path"
+
+	"github.com/milvus-io/birdwatcher/models"
+	"github.com/milvus-io/birdwatcher/proto/v2.2/etcdpb"
+	"github.com/samber/lo"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+const (
+	// DataBaseMetaPrefix is prefix for rootcoord database meta.
+	DataBaseMetaPrefix = `root-coord/database/db-info`
+)
+
+// ListDatabase returns all database info from etcd meta converted to models.
+func ListDatabase(ctx context.Context, cli clientv3.KV, basePath string) ([]*models.Database, error) {
+	prefix := path.Join(basePath, DataBaseMetaPrefix)
+	dbs, keys, err := ListProtoObjects(ctx, cli, prefix, func(*etcdpb.DatabaseInfo) bool {
+		return true
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	result := lo.Map(dbs, func(db etcdpb.DatabaseInfo, idx int) *models.Database {
+		return models.NewDatabase(&db, keys[idx])
+	})
+	return result, nil
+}
