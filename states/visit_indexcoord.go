c169481b7234007f50a68da634d82e7f1a643946
Enwei Jiao <enwei.jiao@zilliz.com>
2023-07-10 15:21:24 +0800

    Add describe index
    Signed-off-by: Enwei Jiao <enwei.jiao@zilliz.com>

--- states/visit_indexcoord.go
+++ states/visit_indexcoord.go
@@ -1,11 +1,13 @@
 package states
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/milvus-io/birdwatcher/models"
 	"github.com/milvus-io/birdwatcher/proto/v2.0/indexpb"
 	indexpbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/indexpb"
+	"github.com/milvus-io/birdwatcher/states/etcd/common"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
 )
@@ -28,6 +30,8 @@ func (s *indexCoordState) SetupCommands() {
 		getMetricsCmd(s.client),
 		// configuration
 		getConfigurationCmd(s.clientv2, s.session.ServerID),
+		// build index progress
+		getDescribeIndex(s.clientv2, s.session.ServerID),
 		//back
 		getBackCmd(s, s.prevState),
 		// exit
@@ -56,4 +60,44 @@ func getIndexCoordState(client indexpb.IndexCoordClient, conn *grpc.ClientConn, 
 	state.SetupCommands()
 
 	return state
+}
+
+func getDescribeIndex(client indexpbv2.IndexCoordClient, serverID int64) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "describe",
+		Run: func(cmd *cobra.Command, args []string) {
+			collectionID, err := cmd.Flags().GetInt64("collection")
+			if err != nil {
+				fmt.Println("empty collectionID, err:", err.Error())
+				cmd.Usage()
+				return
+			}
+			resp, err := client.DescribeIndex(context.Background(), &indexpbv2.DescribeIndexRequest{
+				CollectionID: collectionID,
+			})
+			if err != nil {
+				fmt.Println("failed to call grpc, err:", err.Error())
+				return
+			}
+			for _, info := range resp.GetIndexInfos() {
+				printIndexV2(info)
+			}
+		},
+	}
+	cmd.Flags().Int64("collection", 0, "collection id")
+	return cmd
+}
+
+func printIndexV2(index *indexpbv2.IndexInfo) {
+	fmt.Println("==================================================================")
+	fmt.Printf("Index ID: %d\tIndex Name: %s\tCollectionID:%d\n", index.GetIndexID(), index.GetIndexName(), index.GetCollectionID())
+	fmt.Printf("Indexed Rows: %d\n", index.GetIndexedRows())
+	fmt.Printf("Total Rows: %d\n", index.GetTotalRows())
+	indexParams := index.GetIndexParams()
+	fmt.Printf("Index Type: %s\tMetric Type: %s\n",
+		common.GetKVPair(indexParams, "index_type"),
+		common.GetKVPair(indexParams, "metric_type"),
+	)
+	fmt.Printf("Index Params: %s\n", common.GetKVPair(index.GetUserIndexParams(), "params"))
+	fmt.Println("==================================================================")
 }
