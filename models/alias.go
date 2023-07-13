e98282bb2393ca51ed29388b876a6170820eb9e3
Congqi Xia <congqi.xia@zilliz.com>
2023-07-13 18:30:35 +0800

    Add show alias command
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

---
+++ models/alias.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	etcdpbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/etcdpb"
+)
+
+type Alias struct {
+	Name         string
+	CollectionID int64
+	State        AliasState
+	CreateTS     uint64
+	DBID         int64
+
+	key string
+}
+
+func NewAlias(info *etcdpbv2.AliasInfo, key string) *Alias {
+	a := &Alias{
+		Name:         info.GetAliasName(),
+		CollectionID: info.GetCollectionId(),
+		State:        AliasState(info.GetState()),
+		CreateTS:     info.GetCreatedTime(),
+		DBID:         info.GetDbId(),
+	}
+	return a
+}
+
+type AliasState int32
+
+const (
+	AliasStateAliasCreated  AliasState = 0
+	AliasStateAliasCreating AliasState = 1
+	AliasStateAliasDropping AliasState = 2
+	AliasStateAliasDropped  AliasState = 3
+)
+
+var AliasStateName = map[int32]string{
+	0: "AliasCreated",
+	1: "AliasCreating",
+	2: "AliasDropping",
+	3: "AliasDropped",
+}
+
+var AliasStateValue = map[string]int32{
+	"AliasCreated":  0,
+	"AliasCreating": 1,
+	"AliasDropping": 2,
+	"AliasDropped":  3,
+}
+
+func (x AliasState) String() string {
+	return EnumName(AliasStateName, int32(x))
+}
---
+++ states/etcd/common/alias.go
@@ -0,0 +1,55 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"path"
+
+	"github.com/milvus-io/birdwatcher/models"
+	etcdpbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/etcdpb"
+	"github.com/samber/lo"
+	clientv3 "go.etcd.io/etcd/client/v3"
+)
+
+const (
+	// AliasPrefixBefore210 is the legacy meta prefix for milvus before 2.2.0
+	AliasPrefixBefore210 = `root-coord/collection-alias`
+
+	// AliasPrefixWithoutDB is the meta prefix for alias before database.
+	AliasPrefixWithoutDB = `root-coord/aliases`
+
+	// AliasPrefixDB iis the meta prefix for alias with database feature
+	AliasPrefixDB = `root-coord/database/alias`
+)
+
+func ListAliasVersion(ctx context.Context, cli clientv3.KV, basePath string, version string, filters ...func(*models.Alias) bool) ([]*models.Alias, error) {
+	prefixes := []string{
+		path.Join(basePath, AliasPrefixWithoutDB),
+		path.Join(basePath, AliasPrefixDB),
+	}
+
+	switch version {
+	case models.GTEVersion2_2:
+		var result []*models.Alias
+		for _, prefix := range prefixes {
+			infos, keys, err := ListProtoObjects[etcdpbv2.AliasInfo](ctx, cli, prefix)
+			if err != nil {
+				return nil, err
+			}
+
+			result = append(result, lo.FilterMap(infos, func(info etcdpbv2.AliasInfo, idx int) (*models.Alias, bool) {
+				value := models.NewAlias(&info, keys[idx])
+				for _, filter := range filters {
+					if !filter(value) {
+						return nil, false
+					}
+				}
+				return value, true
+			})...)
+		}
+
+		return result, nil
+	default:
+		return nil, errors.New("not supported version")
+	}
+}
