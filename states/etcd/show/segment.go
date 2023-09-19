f8f85b99aa66b2db72a8e40241cda5b1e0a8c285
Congqi Xia <congqi.xia@zilliz.com>
2023-09-19 15:44:27 +0800

    Fix partition filter not working
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/etcd/show/segment.go
+++ states/etcd/show/segment.go
@@ -27,6 +27,7 @@ type SegmentParam struct {
 func (c *ComponentShow) SegmentCommand(ctx context.Context, p *SegmentParam) error {
 	segments, err := common.ListSegmentsVersion(ctx, c.client, c.basePath, etcdversion.GetVersion(), func(segment *models.Segment) bool {
 		return (p.CollectionID == 0 || segment.CollectionID == p.CollectionID) &&
+			(p.PartitionID == 0 || segment.PartitionID == p.PartitionID) &&
 			(p.SegmentID == 0 || segment.ID == p.SegmentID) &&
 			(p.State == "" || segment.State.String() == p.State)
 	})
