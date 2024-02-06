d48f52f495706bd0464f5839bcf736e36327aeb1
congqixia <congqi.xia@zilliz.com>
2024-02-06 17:58:38 +0800

    fix: Get growing segment id when growing info is in GrowingSegments (#245)
    Growing segment could be in `GrowingSegments` or `GrowingSegmentIDs`
    field in LeaderView
    This handles either case when get segment distribution
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/distribution.go
+++ states/distribution.go
@@ -9,6 +9,7 @@ import (
 	querypbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/querypb"
 	"github.com/milvus-io/birdwatcher/states/etcd/common"
 	"github.com/milvus-io/birdwatcher/states/kv"
+	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -77,7 +78,7 @@ func GetDistributionCommand(cli kv.MetaKV, basePath string) *cobra.Command {
 							continue
 						}
 						fmt.Printf("Leader view for channel: %s\n", lv.GetChannel())
-						growings := lv.GetGrowingSegmentIDs()
+						growings := lo.Uniq(lo.Union(lv.GetGrowingSegmentIDs(), lo.Keys(lv.GetGrowingSegments())))
 						fmt.Printf("Growing segments number: %d , ids: %v\n", len(growings), growings)
 					}
 
