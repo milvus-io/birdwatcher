7ddc70144500d0f8a3a4def6a94e8988089af9f4
congqixia <congqi.xia@zilliz.com>
2024-06-07 10:17:45 +0800

    enhance: Add auto detect rootPath for connect command (#271)
    Find instance rootPath is painful, this PR add auto flag to find milvus
    rootPath in etcd.
    If there is only one candidate, connect command will use it
    automatically
    
    ---------
    
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/etcd_connect.go
+++ states/etcd_connect.go
@@ -52,6 +52,7 @@ type ConnectParams struct {
 	RootCA              string `name:"rootCAPem" default:"" desc:"root CA pem file path"`
 	ETCDPem             string `name:"etcdCert" default:"" desc:"etcd tls cert file path"`
 	ETCDKey             string `name:"etcdKey" default:"" desc:"etcd tls key file path"`
+	Auto                bool   `name:"auto" default:"false" desc:"auto detect rootPath if possible"`
 }
 
 func (s *disconnectState) getTLSConfig(cp *ConnectParams) (*tls.Config, error) {
@@ -109,6 +110,21 @@ func (s *disconnectState) ConnectCommand(ctx context.Context, cp *ConnectParams)
 	})
 	if err != nil {
 		return errors.Wrap(err, "failed to connect to etcd")
+	}
+
+	if cp.Auto {
+		candidates, err := findMilvusInstance(ctx, etcdCli)
+		if err != nil {
+			return err
+		}
+		if len(candidates) == 1 {
+			cp.RootPath = candidates[0]
+		} else if len(candidates) > 1 {
+			fmt.Println("multiple possible rootPath find, cannot use auto mode")
+		} else {
+			fmt.Println("failed to find rootPath candidate")
+			return nil
+		}
 	}
 
 	etcdState := getEtcdConnectedState(etcdCli, cp.EtcdAddr, s.config)
