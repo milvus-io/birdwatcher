89193d8ec50e690b9f47cef982038ae6005fe9b1
congqixia <congqi.xia@zilliz.com>
2024-08-23 19:58:50 +0800

    enhance: Support `repair channel-watch` command to set empty schema (#305)
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

---
+++ states/etcd/common/channel_watch.go
@@ -0,0 +1,30 @@
+package common
+
+import (
+	"context"
+
+	"github.com/golang/protobuf/proto"
+	clientv3 "go.etcd.io/etcd/client/v3"
+
+	"github.com/milvus-io/birdwatcher/models"
+	datapbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/datapb"
+	schemapbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/schemapb"
+)
+
+func WriteChannelWatchInfo(ctx context.Context, cli clientv3.KV, basePath string, info *models.ChannelWatch, schema *schemapbv2.CollectionSchema) error {
+	pb := &datapbv2.ChannelWatchInfo{
+		Vchan:     info.VchanV2Pb,
+		StartTs:   info.StartTs,
+		State:     datapbv2.ChannelWatchState(info.State),
+		TimeoutTs: info.TimeoutTs,
+		Schema:    schema, // use passed schema
+		Progress:  info.Progress,
+		OpID:      info.OpID,
+	}
+	bs, err := proto.Marshal(pb)
+	if err != nil {
+		return err
+	}
+	_, err = cli.Put(ctx, info.Key(), string(bs))
+	return err
+}
