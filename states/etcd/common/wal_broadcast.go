23f8ce3bbe77f0ad28d0ac3a39ff79e2da9c5696
Zhen Ye <chyezh@outlook.com>
2025-11-25 00:43:02 +0800

    enhance: support show/repair broadcast task (#443)
    issue: #406
    
    Signed-off-by: chyezh <chyezh@outlook.com>

--- states/etcd/common/wal_broadcast.go
+++ states/etcd/common/wal_broadcast.go
@@ -3,6 +3,10 @@ package common
 import (
 	"context"
 	"path"
+	"strconv"
+
+	"github.com/cockroachdb/errors"
+	"google.golang.org/protobuf/proto"
 
 	"github.com/milvus-io/birdwatcher/states/kv"
 	"github.com/milvus-io/milvus/pkg/v2/proto/streamingpb"
@@ -12,6 +16,8 @@ const (
 	walBroadcastPrefix = "streamingcoord-meta/broadcast-task/"
 )
 
+var ErrBroadcastTaskNotFound = errors.New("broadcast task not found")
+
 // ListWalBroadcast list broadcast tasks.
 func ListWalBroadcast(ctx context.Context, cli kv.MetaKV, basePath string) ([]*streamingpb.BroadcastTask, error) {
 	prefix := path.Join(basePath, walBroadcastPrefix) + "/"
@@ -20,4 +26,33 @@ func ListWalBroadcast(ctx context.Context, cli kv.MetaKV, basePath string) ([]*s
 		return nil, err
 	}
 	return metas, nil
+}
+
+// ListWalBroadcastByID list broadcast task by broadcast ID.
+func ListWalBroadcastByID(ctx context.Context, cli kv.MetaKV, basePath string, broadcastID int64) (*streamingpb.BroadcastTask, error) {
+	prefix := path.Join(basePath, walBroadcastPrefix, strconv.FormatInt(broadcastID, 10))
+	metas, _, err := ListProtoObjects[streamingpb.BroadcastTask](ctx, cli, prefix)
+	if err != nil {
+		return nil, err
+	}
+	if len(metas) == 0 {
+		return nil, ErrBroadcastTaskNotFound
+	}
+	return metas[0], nil
+}
+
+// SaveWalBroadcastTask reset wal broadcast task.
+func SaveWalBroadcastTask(ctx context.Context, cli kv.MetaKV, basePath string, broadcastID int64, broadcastTask *streamingpb.BroadcastTask) error {
+	prefix := path.Join(basePath, walBroadcastPrefix, strconv.FormatInt(broadcastID, 10))
+	bs, err := proto.Marshal(broadcastTask)
+	if err != nil {
+		return err
+	}
+	return cli.Save(ctx, prefix, string(bs))
+}
+
+// RemoveWalBroadcastTask remove wal broadcast task.
+func RemoveWalBroadcastTask(ctx context.Context, cli kv.MetaKV, basePath string, broadcastID int64) error {
+	prefix := path.Join(basePath, walBroadcastPrefix, strconv.FormatInt(broadcastID, 10))
+	return cli.RemoveWithPrefix(ctx, prefix)
 }
--- states/etcd/show/wal_broadcast.go
+++ states/etcd/show/wal_broadcast.go
@@ -2,24 +2,44 @@ package show
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/jedib0t/go-pretty/v6/table"
+	"google.golang.org/protobuf/encoding/protojson"
 
 	"github.com/milvus-io/birdwatcher/framework"
 	"github.com/milvus-io/birdwatcher/states/etcd/common"
+	"github.com/milvus-io/milvus/pkg/v2/proto/streamingpb"
 	"github.com/milvus-io/milvus/pkg/v2/streaming/util/message"
 )
 
 type WalBroadcastParam struct {
 	framework.ParamBase `use:"show wal-broadcast" desc:"display wal broadcast information from coordinator meta store"`
+	BroadcastID         int64 `name:"broadcast_id" default:"" desc:"broadcast id to show"`
+	Detail              bool  `name:"detail" default:"false" desc:"display detail information of broadcast task"`
 }
 
 func (c *ComponentShow) WalBroadcastCommand(ctx context.Context, p *WalBroadcastParam) error {
-	metas, err := common.ListWalBroadcast(ctx, c.client, c.metaPath)
-	if err != nil {
-		return err
+	var metas []*streamingpb.BroadcastTask
+	if p.BroadcastID != 0 {
+		meta, err := common.ListWalBroadcastByID(ctx, c.client, c.metaPath, p.BroadcastID)
+		if err != nil {
+			return err
+		}
+		metas = append(metas, meta)
+	} else {
+		var err error
+		if metas, err = common.ListWalBroadcast(ctx, c.client, c.metaPath); err != nil {
+			return err
+		}
 	}
+	if p.Detail {
+		c.printDetail(metas)
+		return nil
+	}
+
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.SetTitle("WAL Broadcast At Coordinator")
@@ -31,15 +51,47 @@ func (c *ComponentShow) WalBroadcastCommand(ctx context.Context, p *WalBroadcast
 		for rk := range bh.ResourceKeys {
 			rks = append(rks, rk.String())
 		}
-		// TODO: add message details
+		acked, bitmap := c.getAcked(meta)
 		t.AppendRow(table.Row{
 			bh.BroadcastID,
 			msg.MessageType().String(),
 			rks,
-			len(meta.AckedCheckpoints),
+			fmt.Sprintf("(%08b)%d/%d", bitmap, acked, len(bh.VChannels)),
 			meta.State.String(),
 		})
 	}
 	t.Render()
 	return nil
+}
+
+func (c *ComponentShow) getAcked(meta *streamingpb.BroadcastTask) (int, []byte) {
+	acked := 0
+	bitmap := make([]byte, len(meta.AckedCheckpoints))
+	for idx, checkpoint := range meta.AckedCheckpoints {
+		if checkpoint != nil && checkpoint.TimeTick != 0 {
+			acked++
+			bitmap[idx] = 1
+		}
+	}
+	return acked, bitmap
+}
+
+func (c *ComponentShow) printDetail(metas []*streamingpb.BroadcastTask) {
+	for _, meta := range metas {
+		msg := message.NewBroadcastMutableMessageBeforeAppend(meta.Message.Payload, meta.Message.Properties)
+		bh := msg.BroadcastHeader()
+		rks := make([]string, 0, len(bh.ResourceKeys))
+		for rk := range bh.ResourceKeys {
+			rks = append(rks, rk.String())
+		}
+		metaJSON, _ := protojson.Marshal(meta)
+		acked, bitmap := c.getAcked(meta)
+		fmt.Printf("=======Broadcast ID: %d====================\n", bh.BroadcastID)
+		fmt.Printf("MessageType: %s\n", msg.MessageType().String())
+		fmt.Printf("State: %s\n", meta.State.String())
+		fmt.Printf("ResourceKeys: %s\n", strings.Join(rks, ", "))
+		fmt.Printf("VChannels: %s\n", strings.Join(bh.VChannels, ", "))
+		fmt.Printf("Acked: %s\n", fmt.Sprintf("(%08b)%d/%d", bitmap, acked, len(bh.VChannels)))
+		fmt.Printf("Detail: \n%s\n\n", string(metaJSON))
+	}
 }
