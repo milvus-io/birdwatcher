86be442211f1edabf7cfd1af07facd1d5b3fd9c7
Congqi Xia <congqi.xia@zilliz.com>
2025-12-18 18:21:45 +0800

    Fix lint and logic
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/etcd/repair/mixed_binlogs.go
+++ states/etcd/repair/mixed_binlogs.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/cockroachdb/errors"
+	"github.com/samber/lo"
+
 	"github.com/milvus-io/birdwatcher/framework"
 	"github.com/milvus-io/birdwatcher/models"
 	"github.com/milvus-io/birdwatcher/states/etcd/common"
 	"github.com/milvus-io/milvus/pkg/v2/util/typeutil"
-	"github.com/samber/lo"
 )
 
 type MixedBinlogsParam struct {
@@ -26,6 +28,18 @@ func (c *ComponentRepair) MixedBinlogsCommand(ctx context.Context, p *MixedBinlo
 		segment          *models.Segment
 		duplicatedFields []int64
 		targetBinlogs    []*models.FieldBinlog
+		verdict          string
+	}
+
+	collections := make(map[int64]*models.Collection)
+	getCollection := func(collID int64) (*models.Collection, error) {
+		coll, ok := collections[collID]
+		if ok {
+			return coll, nil
+		}
+		var err error
+		coll, err = common.GetCollectionByIDVersion(ctx, c.client, c.basePath, collID)
+		return coll, err
 	}
 
 	var results []*target
@@ -33,20 +47,42 @@ func (c *ComponentRepair) MixedBinlogsCommand(ctx context.Context, p *MixedBinlo
 		v1 := typeutil.NewSet[int64]()
 		v2 := typeutil.NewSet[int64]()
 		v1binlogs := make(map[int64]*models.FieldBinlog)
+		v2binlogs := make(map[int64]*models.FieldBinlog)
+
 		for _, binlog := range segment.GetBinlogs() {
 			if len(binlog.ChildFields) > 0 {
 				v2.Insert(binlog.ChildFields...)
+				v2binlogs[binlog.FieldID] = binlog
 			} else {
 				v1.Insert(binlog.FieldID)
 				v1binlogs[binlog.FieldID] = binlog
 			}
 		}
+
 		result := v1.Intersection(v2)
 		if result.Len() > 0 {
+			coll, err := getCollection(segment.CollectionID)
+			if err != nil {
+				return err
+			}
+			fieldNumber := len(coll.GetProto().Schema.Fields)
+			var targetBinlogs []*models.FieldBinlog
+			var verdict string
+			switch {
+			case fieldNumber == v1.Len():
+				targetBinlogs = lo.Values(v2binlogs)
+				verdict = "v1 haves all fields, remove v2 binlogs"
+			case fieldNumber == v2.Len():
+				targetBinlogs = lo.Values(v1binlogs)
+				verdict = "v2 haves all fields, remove v1 binlogs"
+			default:
+				return errors.New("neither v1 or v2 binlog has all fields")
+			}
 			results = append(results, &target{
 				segment:          segment,
 				duplicatedFields: result.Collect(),
-				targetBinlogs:    lo.Map(result.Collect(), func(fieldID int64, _ int) *models.FieldBinlog { return v1binlogs[fieldID] }),
+				targetBinlogs:    targetBinlogs,
+				verdict:          verdict,
 			})
 		}
 	}
@@ -56,6 +92,7 @@ func (c *ComponentRepair) MixedBinlogsCommand(ctx context.Context, p *MixedBinlo
 		for _, result := range results {
 			fmt.Printf("Segment %d has both v1 & v2 binlog records\n", result.segment.GetID())
 			fmt.Printf("Duplicated fields: %v\n", result.duplicatedFields)
+			fmt.Println(result.verdict)
 			segment := result.segment
 			for _, binlog := range result.targetBinlogs {
 				fmt.Println("plan to remove key: ", fmt.Sprintf("%s/datacoord-meta/binlog/%d/%d/%d/%d", c.basePath, segment.CollectionID, segment.PartitionID, segment.ID, binlog.FieldID))
@@ -67,6 +104,7 @@ func (c *ComponentRepair) MixedBinlogsCommand(ctx context.Context, p *MixedBinlo
 	for _, result := range results {
 		fmt.Printf("Segment %d has both v1 & v2 binlog records\n", result.segment.GetID())
 		fmt.Printf("Duplicated fields: %v\n", result.duplicatedFields)
+		fmt.Println(result.verdict)
 		segment := result.segment
 		for _, binlog := range result.targetBinlogs {
 			key := fmt.Sprintf("%s/datacoord-meta/binlog/%d/%d/%d/%d", c.basePath, segment.CollectionID, segment.PartitionID, segment.ID, binlog.FieldID)
